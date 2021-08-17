2830b9c023a23189b7ababbb9bdcb927178270e1
Mfernandes86 <87205532+Mfernandes86@users.noreply.github.com>
2021-08-17 09:32:41 +0200

    Create main terraform test ubuntu 20.04 deploy

---
+++ main.tf
@@ -0,0 +1,21 @@
+variable "region" {}
+variable "shared_credentials_file" {}
+variable "profile" {}
+variable "key_name" {}
+
+provider "aws" {
+  shared_credentials_file = "${var.shared_credentials_file}"
+  region     = "${var.region}"
+  profile    = "${var.profile}"
+}
+
+resource "aws_instance" "test" {
+  ami           = "ami-6b3fd60c"
+  instance_type = "t2.micro"
+  key_name   = "${var.key_name}"
+  tags {
+    Name = "tst-ubuntu-20.04"
+    Terraform = "true"
+    Environment = "Dev"
+  }
+}
